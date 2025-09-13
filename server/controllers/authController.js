d3cf3cdd69645a00db8e9db603c3f6d07a969241
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-09-13 20:04:29 +0300

    first commit

---
+++ server/controllers/authController.js
@@ -0,0 +1,92 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
+exports.signup = async (req, res) => {
+  try {
+    const { email, password, userType } = req.body;
+
+    // 1) Check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'User already exists with this email'
+      });
+    }
+
+    // 2) Create new user
+    const newUser = await User.create({
+      email,
+      password,
+      userType
+    });
+
+    // 3) Remove password from output
+    newUser.password = undefined;
+
+    // 4) Generate JWT token
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+      status: 'success',
+      token,
+      data: {
+        user: newUser
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Please provide email and password'
+      });
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+    
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect email or password'
+      });
+    }
+
+    // 3) Remove password from output
+    user.password = undefined;
+
+    // 4) Generate JWT token
+    const token = signToken(user._id);
+
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message
+    });
+  }
+};
